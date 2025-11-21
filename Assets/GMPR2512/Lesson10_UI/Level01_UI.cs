12210604e3a26a341c03ffc5747f56b2c827246f
cnobert <cnobert@nait.ca>
2025-11-21 09:01:23 -0700

    level 1

--- Assets/GMPR2512/Lesson10_UI/Level01_UI.cs
+++ Assets/GMPR2512/Lesson10_UI/Level01_UI.cs
@@ -9,14 +9,12 @@ namespace Lesson10_UI
         [SerializeField] private GameState _gameState;
         private Label _scoreLabel;
 
-        private float _delaySeconds = 2.0f;
+        private float _delaySeconds = 5.0f;
         private float _timer;
-        private bool _menuLoaded;
 
         private void Awake()
         {
             _timer = 0f;
-            _menuLoaded = false;
         }
 
         private void OnEnable()
@@ -29,19 +27,16 @@ namespace Lesson10_UI
         {
             _scoreLabel.text = _gameState._scoreLevel01.ToString();
 
-            if (_menuLoaded == false)
+            _timer += Time.deltaTime;
+            if (_timer >= _delaySeconds)
             {
-                _timer += Time.deltaTime;
-                if (_timer >= _delaySeconds)
-                {
-                    _menuLoaded = true;
-                    TestLoadMenu();
-                }
+                TestLoadMenu();
             }
         }
 
         private void TestLoadMenu()
         {
+            
             SceneManager.LoadScene("Lesson10_UI");
         }
     }
